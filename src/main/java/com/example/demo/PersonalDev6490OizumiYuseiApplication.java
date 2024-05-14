eb241d6f44aebca3fa53e3b7c34af0b61416dfcd
oizumi-yusei-6490 <420-student002@la-study.com>
2024-05-14 15:33:15 +0900

    ファーストコミット

---
+++ src/main/java/com/example/demo/PersonalDev6490OizumiYuseiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev6490OizumiYuseiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev6490OizumiYuseiApplication.class, args);
+	}
+
+}
